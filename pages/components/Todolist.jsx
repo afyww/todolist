28a293bcd53ab432890f9a70f6d7ee49a8e57992
Afy Wahyu <afyww18@gmail.com>
2024-01-02 17:28:54 +0700

    ....

--- pages/components/Todolist.jsx
+++ pages/components/Todolist.jsx
@@ -3,10 +3,10 @@ import Link from 'next/link'
 
 const Todolist = () => {
   return (
-<div className='mx-auto w-fit space-y-4 my-auto bg-white p-10 xl:p-24 rounded-3xl'>
+<div className='mx-auto space-y-4 my-auto bg-white p-6 xl:p-24 rounded-3xl'>
     <h1 className='xl:text-6xl text-3xl font-bold'>Todo List</h1>
     <div className='gap-3 flex'>
-    <input placeholder='Add your new todo w-full' className='border-2 p-2 w-full' type="text" />
+    <input placeholder='Add your new todo' className='border-2 p-2 w-full' type="text" />
     <button className='text-light bg-blue-700 p-3 text-white'>Add</button>
     </div>
     <div className=''>
